c74bc55c294fbb186c3b4c7a11b8c874a4c96510
Senzo Khoza <sphiwe@thedigitalacademy.co.za>
2020-07-10 10:24:44 +0200

    adding javascript

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const navToggle = document.querySelector('.navToggle');
+const navToggle = document.querySelector('.nav-toggle');
 
 navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('.nav-open');
-})
+    document.body.classList.toggle('nav-open');
+});
